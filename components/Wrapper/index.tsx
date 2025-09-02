d97b1011bfec7a99cafaa1c8b9d63e5afcaf1f5c
wid46do <rubenemmanuel2000w@gmail.com>
2025-09-02 22:18:31 +0700

    add firebase

--- components/Wrapper/index.tsx
+++ components/Wrapper/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Header from "@components/Header";
 
@@ -10,7 +10,7 @@ const Wrapper: React.FC<Props> = ({ children }) => {
   return (
     <div>
       <Header />
-      {children}
+      <div className="pt-20">{children}</div>
     </div>
   );
 };
